30d9203ccdf363a5ed3c110941f15eaa3564f224
Viktoriya Diukina <viki_dyukina@mail.ru>
2024-11-19 21:01:03 +0400

    created templates for passwords

--- account/forms.py
+++ account/forms.py
@@ -43,3 +43,10 @@ class UserUpdateForm(forms.ModelForm):
         model = User
         fields = ['username', 'email']
         exclude = ('password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data['email'].lower()
+        if User.objects.filter(email=email).exclude(id=self.instance.id).exists() or len(email) > 254:
+            raise forms.ValidationError("Email is already in use or too long")
+
+        return email
--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.urls import path, reverse_lazy
 from django.shortcuts import render
 from . import views
 
@@ -15,5 +16,23 @@ urlpatterns = [
     path('dashboard/', views.dashboard_user, name='dashboard'),
     path('profile-management/', views.profile_user, name='profile-management'),
     path('delete-user', views.delete_user, name='delete-user'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='account/password/password-reset.html',
+        email_template_name='account/password/password_reset_email.html',
+        success_url=reverse_lazy('account:password_reset_done')),
+         name='password_reset'),
+
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='account/password/password-reset-done.html'),
+         name='password_reset_done'),
+
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='account/password/password-reset-confirm.html',
+        success_url=reverse_lazy('account:password_reset_complete')),
+         name='password_reset_confirm'),
+
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='account/password/password-reset-complete.html'),
+         name='password_reset_complete'),
 ]
 
